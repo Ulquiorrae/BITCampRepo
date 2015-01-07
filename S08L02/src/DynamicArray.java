8c04b2065f50eedcf8cf6b9930a025ab2621d828
Uqluiorrae <hariskrkalic@gmail.com>
2015-01-07 09:27:55 +0100

    added comments on a DynamicArray class

--- S08L02/src/DynamicArray.java
+++ S08L02/src/DynamicArray.java
@@ -4,16 +4,28 @@ public class DynamicArray {
 	private int count;
 	final int INIT_SIZE = 5;
 
+	/**
+	 * Constructor for DynamicArray class.
+	 * @param size
+	 */
 	public DynamicArray(int size) {
 		this.array = new int[size];
 		this.count = 0;
 	}
 
+	/**
+	 * Constructor for DynamicArray class which does not recieve any parameters.
+	 * @param size
+	 */
 	public DynamicArray() {
 		this.array = new int[INIT_SIZE];
 		this.count = 0;
 	}
 
+	/**
+	 * This method returns the size of an array.
+	 * @return integer
+	 */
 	public int getSize() {
 		return count;
 	}
@@ -34,6 +46,11 @@ public class DynamicArray {
 	// array = temp;
 	// }
 
+	
+	/**
+	 * This method adds a new element to an array of integers.
+	 * @param newElement
+	 */
 	public void add(int newElement) {
 		if (count == array.length)
 			resize();
@@ -41,6 +58,9 @@ public class DynamicArray {
 		count++;
 	}
 
+	/**
+	 * This method resizes an array of integers(doubles it).
+	 */
 	private void resize() {
 		int[] tmp = array;
 		array = new int[tmp.length * 2];
@@ -49,6 +69,11 @@ public class DynamicArray {
 		}
 	}
 
+	/**
+	 * This method returns a value of an element on an index it recieves as a parameter.
+	 * @param index
+	 * @return integer - value of an element on index "index".
+	 */
 	private int getAt(int index) {
 
 		if (index > count || index < 0) {
@@ -58,6 +83,10 @@ public class DynamicArray {
 		}
 	}
 
+	/**
+	 * This method removes an element from an array of integers which is located on an index the method recieves as a parameter.
+	 * @param index
+	 */
 	private void removeAt(int index) {
 
 		if (index > count || index < 0) {
@@ -71,6 +100,10 @@ public class DynamicArray {
 		}
 	}
 
+	/**
+	 * This method creates an array of integers from an DynamicArray
+	 * @return array of integers
+	 */
 	private int[] toArray() {
 		int[] newArray = new int[count];
 		for (int i = 0; i < count; i++) {
