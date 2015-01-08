e74540eef286d64df2b40e92cd1cada3c01f5896
Uqluiorrae <hariskrkalic@gmail.com>
2015-01-08 12:26:40 +0100

    added interface

--- S08L04/src/PersonList.java
+++ S08L04/src/PersonList.java
@@ -1,5 +1,7 @@
+import java.io.ObjectInputStream.GetField;
 
-public class PersonList {
+
+public class PersonList implements PersonListInterface {
 
 	private PersonNode firstPerson;
 	
@@ -10,11 +12,10 @@ public class PersonList {
 	}
 	
 	
-	/**
-	 * This method adds a new element on a linked list.
-	 * @param person 
-	 * @param firstPerson
+	/* (non-Javadoc)
+	 * @see PersonListInterface#add(Person)
 	 */
+	@Override
 	public void add(Person person) {
 
 		PersonNode next = new PersonNode(person);
@@ -28,6 +29,55 @@ public class PersonList {
 			lastPerson.setNext(next);
 		}
 	}
+	
+	/* (non-Javadoc)
+	 * @see PersonListInterface#printList()
+	 */
+	@Override
+	public void printList() {
+
+		// Prints all elements of a linked list on a screen.
+		System.out.println("Names in the list: ");
+		PersonNode current = getFirstPerson();
+		while (current != null) {
+			System.out.println(current.getP().getName());
+			current = current.getNext();
+		}
+
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see PersonListInterface#countListElements()
+	 */
+	@Override
+	public int countListElements() {
+		int counter = 0;
+		PersonNode current = getFirstPerson();
+		while (current != null) {
+			current = current.getNext();
+			counter++;
+		}
+		return counter;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see PersonListInterface#toArray()
+	 */
+	@Override
+	public Person[] toArray() {
+		Person[] newArray = new Person[countListElements()];
+		int i = 0;
+		PersonNode current = firstPerson;
+		while (current != null) {
+			newArray[i] = current.getP();
+			current = current.getNext();
+			i++;
+		}
+		return newArray;
+	}
+	
 }
 
 
